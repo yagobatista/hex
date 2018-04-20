6eeb0af7436612eec8448401882f7112e38e038f
yago batista da silva <yagobatistasilva@gmail.com>
2018-04-20 20:59:57 -0300

    atualizando nomes para hexagono

--- js/helpers.js
+++ js/helpers.js
@@ -2,22 +2,22 @@ function makeBoard(column) {
     for (let i = 0; i < column; i++) {
         document.getElementsByClassName('board')[0].innerHTML += '<div class="linha">';
         for (let i = 0; i < column; i++) {
-            document.getElementsByClassName('board')[0].innerHTML += '<div class="lozango"></div>';
+            document.getElementsByClassName('board')[0].innerHTML += '<div class="hexagono"></div>';
         }
         document.getElementsByClassName('board')[0].innerHTML += '</div>';
     }
 }
 function renderBoard(board) {
     // function to render de array of array witch is de board in de screen
-    for (let i = 0; i < board[0].length; i++) {
+    for (let i = 0; i < 8; i++) {
         document.getElementsByClassName('board')[0].innerHTML += '<div class="linha">';
-        for (let j = 0; j < board[i].length; j++) {
-            document.getElementsByClassName('board')[0].innerHTML += '<div class="lozango"></div>';
+        for (let j = 0; j < 8; j++) {
+            document.getElementsByClassName('board')[0].innerHTML += '<div class="hexagono"></div>';
         }
         document.getElementsByClassName('board')[0].innerHTML += '</div>';
     }
 }
 function markPosition(board,line, column) {
-    const lozangos = document.getElementsByClassName('lozango');
-    lozangos[index].className = 'lozango selected';   
+    const hexagonos = document.getElementsByClassName('hexagono');
+    hexagonos[index].className = 'hexagono selected';   
 }
