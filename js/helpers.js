fd4e305a02c5f3af71776c31491349b95094e8cb
yago batista da silva <yagobatistasilva@gmail.com>
2018-04-21 17:02:31 -0300

    escolher o tamanho do board

--- js/helpers.js
+++ js/helpers.js
@@ -1,4 +1,5 @@
 function makeBoard(column) {
+    //render of t
     var boardString = '';
     for (let i = 0; i < column; i++) {
          boardString += '<div class="hex-row n' + i + '">';
@@ -8,6 +9,21 @@ function makeBoard(column) {
         boardString += '</div>';
     }
     document.getElementsByClassName('board')[0].innerHTML = boardString;
+    //evento de click
+    const hexagonos = document.getElementsByClassName('hex');
+    for (let index = 0; index < hexagonos.length; index++) {
+        hexagonos[index].onclick = function () {
+            // debugger;
+            // var line = index / 8;
+            // var column = index % 8;
+            // board = markPosition(board,line, column);
+            hexagonos[index].className = 'hex selected';
+            // if(isInLastStage(hexagonos)) {
+            //     alert('você é um ganhador!');
+            // }
+        };
+
+    }
 }
 function renderBoard(board) {
     // function to render de array of array witch is de board in de screen
