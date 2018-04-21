c626cfcd61499788e50f3e336e525d1556cbc5b4
yago batista da silva <yagobatistasilva@gmail.com>
2018-04-21 15:37:01 -0300

    board com hexagonos

--- js/helpers.js
+++ js/helpers.js
@@ -1,11 +1,13 @@
 function makeBoard(column) {
+    var boardString = '';
     for (let i = 0; i < column; i++) {
-        document.getElementsByClassName('board')[0].innerHTML += '<div class="linha">';
-        for (let i = 0; i < column; i++) {
-            document.getElementsByClassName('board')[0].innerHTML += '<div class="hexagono"></div>';
+         boardString += '<div class="hex-row n' + i + '">';
+        for (let j = 0; j < column; j++) {
+            boardString += ' <div class="hex"> <div class="top"></div> <div class="middle"></div> <div class="bottom"></div> </div>';
         }
-        document.getElementsByClassName('board')[0].innerHTML += '</div>';
+        boardString += '</div>';
     }
+    document.getElementsByClassName('board')[0].innerHTML = boardString;
 }
 function renderBoard(board) {
     // function to render de array of array witch is de board in de screen
