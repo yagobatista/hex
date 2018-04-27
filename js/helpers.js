f1371c5756e09f294c930571dbd94cd45668a374
Marco Bonetti <marco_bonetti@uol.com.br>
2018-04-27 12:10:06 -0300

    calcula vitoria do jogador e da ia

--- js/helpers.js
+++ js/helpers.js
@@ -21,6 +21,7 @@ function makeBoard(board) {
     }
 }
 
+
 function markPosition(board, line, column, player = null) {
     player = player && `${ player }-` || '';
 	alert(player)
@@ -28,4 +29,108 @@ function markPosition(board, line, column, player = null) {
     const hexagonos = document.getElementsByClassName('hex');
     board[line][column] = positionValue;
     hexagonos[(line * board.length) + column].className = `hex ${ player }selected`;
+}
+
+function geraCandidatos(linha, coluna, dimensao){
+	//forma os candidatos
+	candidatos = new Array()
+	if (linha > 0){
+		candidatos.push([linha-1, coluna])
+	}
+	if (linha > 0 && coluna < dimensao){
+		candidatos.push([linha-1, coluna+1])
+	}
+	if (coluna > 0){
+		candidatos.push([linha, coluna-1])
+	}
+	if (coluna < dimensao){
+		candidatos.push([linha, coluna+1])
+	}
+	if (linha < dimensao && coluna > 0){
+		candidatos.push([linha+1, coluna-1])
+	}
+	if (linha < dimensao){
+		candidatos.push([linha+1, coluna])
+	}
+	return candidatos
+}
+
+function eliminaRedundancia (linha, coluna, filhos, corte){
+	var um
+	var dois
+	escolhidos = new Array()
+	if (filhos) {
+	//retira os nós já visitados
+		for(let i=0; i < filhos.length; i++){
+			let recusa = false
+			for(let j=0; j < corte.length-1; j++){
+				um = ""+filhos[i]
+				dois = ""+corte[j]
+				if (um == dois){
+					recusa = true
+				}
+			}
+			if(!recusa){ 
+				escolhidos.push(filhos[i])
+			} 
+		}
+	}
+	return escolhidos
+}
+
+function checaFilho(escolhidos, player, board, corta){
+//vê se o filho tem peça do jogador
+	caminho = new Array()
+	for(let i=0; i<escolhidos.length; i++) {
+		if(player == board[escolhidos[i][0]][escolhidos[i][1]]){
+			caminho = escolhidos[i]
+		} else {
+			corta.push(escolhidos[i])
+		}
+	}
+	if(caminho.length > 0){
+		return caminho
+	} else {
+		return false
+	}
+}
+
+
+function buscaProfundidade(linha, coluna, board, player, corte){
+//vê se o jogador tem um caminho de ponta a ponta
+	if ((player > 0 && coluna == 0) || (player < 0 && linha == 0)) {
+		return true
+	} else {
+				corte.push([linha, coluna])
+				filhos = geraCandidatos(linha, coluna, board.length-1)
+				escolhidos = eliminaRedundancia(linha, coluna, filhos, corte)
+				caminho = checaFilho(escolhidos, player, board, corte)
+				if(caminho){
+					for(let i=0; i < caminho.length-1; i++){
+						return buscaProfundidade(caminho[i], caminho[i+1], board, player, corte)
+					}
+				} else { 
+					return false 
+				}
+	}
+}
+function calculaPontos(board, player = 1){
+	let pontos = 0
+	let size = board.length
+	let k = 0
+	   for (let i = 0; i < size; i++) {
+                    for (let j = 0; j < size-1; j++) {
+                        if (player > 0 && board[i][j] == player && ((board[i][j+1] == player) || ( i > 0 && board[i-1][j+1] == player))){
+			pontos += 10 
+			alert(pontos+" ganhos em i = "+i+" e j = "+j+"pelo player = "+board[i][j]+" vulgo "+player+"vizinho de = "+board[i][j+1])
+			}
+                        if (player < 0 && board[i][j] < 0 == player && board[i+1][j] == player && false){
+			pontos += 10 
+			}
+                    }
+		k++
+           }
+	alert(pontos)
+	alert(buscaProfundidade(board.length-1, board.length-1, board, player))
+	return pontos
 }
--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 function playerMove(board, line, column) {
     if (board[line][column] === 0) {
         markPosition(board, line, column);
-        if (gameOver(board)) {
-            alert('Você ganhou!');
-        } else {
+        if (!gameOver(board)) {
             setTimeout(() => {
                 aiMove(board);
             }, 500);
@@ -26,101 +24,16 @@ function aiMove(board) {
 }
 
 function gameOver(board, player = 1) {
+
 	aux = [board.length,board.length]
+	corte = new Array(aux)
 	retorno = false
 	for (let i = board.length-1; i >= 0; i--){
-		if (buscaProfundidade(i,board.length-1, board, player, corte, rodada)){
+		if (((player < 0 && board[board.length-1][i] == player) && (buscaProfundidade(board.length-1, i, board, player, corte)) ) || ((player > 0 && board[i][board.length-1] == player) && (buscaProfundidade(i,board.length-1, board, player, corte)))) {
 			retorno = true;
-		}
+		} 
 	}
 	return retorno;
 }
 
-function geraCandidatos(linha, coluna, dimensao){
-	//forma os candidatos
-	alert("linha")
-	alert(linha)
-	alert("coluna")
-	alert(coluna)
-	candidatos = new Array()
-	escolhidos = new Array()
-	if (linha > 0){
-		candidatos.push([linha-1, coluna])
-	}
-	if (linha > 0 && coluna < dimensao){
-		candidatos.push([linha-1, coluna+1])
-	}
-	if (coluna > 0){
-		candidatos.push([linha, coluna-1])
-	}
-	if (coluna < dimensao){
-		candidatos.push([linha, coluna+1])
-	}
-	if (linha < dimensao && coluna > 0){
-		candidatos.push([linha+1, coluna-1])
-	}
-	if (linha < dimensao){
-		candidatos.push([linha+1, coluna])
-	}
-	return candidatos
-}
 
-function eliminaRedundancia (linha, coluna, filhos, corte){
-	if (filhos) {
-	//retira os nós já visitados
-		for(let i=0; i < filhos.length; i++){
-			recusa = false
-			for(let j=0; j < filhos.length; j++){
-				if (candidatos[i] == corte[j]){
-					recusa = true
-				}
-			}
-			if(!recusa){ 
-				escolhidos.push(filhos[i])
-			} else {
-					corte.push(filhos[i])
-			}
-		}
-	}
-	return escolhidos
-}
-
-function buscaProfundidade(linha, coluna, board, player, corte, rodada){
-	if (coluna == 0) {
-		return true
-	} else {	
-				aux = new Array()
-				corte = new Array(aux)
-				escolhidos = new Array(aux)
-				filhos.push([linha, coluna])
-				filhos.push(geraCandidatos(linha, coluna, board.length-1))
-				alert("filhos = "+ filhos)
-				escolhidos = eliminaRedundancia(linha, coluna, filhos, corte)
-				alert("escolhidos = "+escolhidos)
-				for(let i=0; i < filhos.length-1; i++){
-					return buscaProfundidade(filhos[i][0], filhos[i][1], board, player, corte)
-				}
-	}
-	return false
-}
-
-function calculaPontos(board, player = 1){
-	let pontos = 0
-	let size = board.length
-	let k = 0
-	   for (let i = 0; i < size; i++) {
-                    for (let j = 0; j < size-1; j++) {
-                        if (player > 0 && board[i][j] == player && ((board[i][j+1] == player) || ( i > 0 && board[i-1][j+1] == player))){
-			pontos += 10 
-			alert(pontos+" ganhos em i = "+i+" e j = "+j+"pelo player = "+board[i][j]+" vulgo "+player+"vizinho de = "+board[i][j+1])
-			}
-                        if (player < 0 && board[i][j] < 0 == player && board[i+1][j] == player && false){
-			pontos += 10 
-			}
-                    }
-		k++
-           }
-	alert(pontos)
-	alert(buscaProfundidade(board.length-1, board.length-1, board, player))
-	return pontos
-}
