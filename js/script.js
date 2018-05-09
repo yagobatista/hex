1dba4495c3d13a39b23743db25978051f49b02a3
Marco Bonetti <marco_bonetti@id.uff.br>
2018-05-09 11:17:46 -0300

    versao ia x ia

--- js/script.js
+++ js/script.js
@@ -13,13 +13,58 @@ function playerMove(board, line, column) {
 
 function aiMove(board) {
     // random ai move
-    par = new Array()
     let line;
     let column;
    do {
         line = parseInt(Math.random() * board.length);
         column = parseInt(Math.random() * board.length);
     } while (board[line][column] !== 0);
+//marca posicao e vê se ganhou
+    markPosition(board, line, column, 'ai');
+}
+
+function aiMinimax(board, player) {
+    // Minimax ai move
+    let line;
+    let column;
+    let par = []
+	par = calculaMinimax(board, player)
+//	par = calculaAvaliacaoComCutOff(board)
+//	alert("par "+JSON.stringify(par))
+	if(par[0]==player){
+	line = par[1][0]
+	column = par[1][1]
+//	alert("linha "+line)
+//	alert("column "+column)
+	} else {
+	   do {
+		line = parseInt(Math.random() * board.length);
+		column = parseInt(Math.random() * board.length);
+	    } while (board[line][column] !== 0);
+	}
+//marca posicao e vê se ganhou
+    markPosition(board, line, column);
+}
+
+function aiCutOff(board, player) {
+    // Minimax ai move
+    let line;
+    let column;
+    let par = []
+//	par = calculaMinimax(board)
+	par = calculaAvaliacaoComCutOff(board)
+//	alert("par "+JSON.stringify(par))
+	if(par[0]==player){
+	line = par[1][0]
+	column = par[1][1]
+//	alert("linha "+line)
+//	alert("column "+column)
+	} else {
+	   do {
+		line = parseInt(Math.random() * board.length);
+		column = parseInt(Math.random() * board.length);
+	    } while (board[line][column] !== 0);
+	}
 //marca posicao e vê se ganhou
     markPosition(board, line, column, 'ai');
 }
